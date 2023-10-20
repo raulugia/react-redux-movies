1e5bb28327f1a88bb4f41ab1ba5cbd23c3c75d75
Raul <raul.ugia@gmail.com>
2023-10-20 19:13:17 +0200

    Home - fetching from API

--- src/common/apis/movieApi.js
+++ src/common/apis/movieApi.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: "https://img.omdbapi.com"
+    baseURL: "https://www.omdbapi.com/"
 })
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import MovieListing from '../MovieListing/MovieListing'
+import movieApi from "../../common/apis/movieApi"
+import {key} from "../../common/apis/movieApiKey"
 
 const Home = () => {
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      const movieText = "Harry"
+      const response = await movieApi.get(
+        `?t=${movieText}&apikey=${key}`
+        )
+        .catch(err => {
+          console.log(err.message || err)
+        })
+      console.log(response)
+    }
+
+    fetchMovies()
+  }, [])
   return (
-    <div>Home</div>
+    <div className="">
+      <div className="banner-img">
+
+      </div>
+      <MovieListing />
+    </div>
   )
 }
 
