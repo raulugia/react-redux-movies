fcf4df7d56aec36892cf3f9e76807ae4f5c661c9
Raul <raul.ugia@gmail.com>
2023-10-20 18:20:50 +0200

    routes created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
-import './App.css'
+import React from 'react';
+import './App.scss'
+import { Router, Route, Routes} from "react-router-dom"
+import Home from "./components/Home/Home"
+import Header from "./components/Header/Header"
+import MovieDetail from "./components/MovieDetail/MovieDetail"
+import PageNotFound from "./components/PageNotFound/PageNotFound"
+import Footer from "./components/Footer/Footer"
 
 function App() {
 
   return (
-    <div>
-      <h1>App</h1>
+    <div className='app'>
+        <Header></Header>
+        <Routes>
+          <Route path="/" exact element={<Home/>} />
+          <Route path="/movie/:imdID" element={<MovieDetail/>} />
+          <Route path="*" element={<PageNotFound/>} />
+        </Routes>
+        <Footer />
     </div>
   )
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="header">
+      <Link to="/">
+        <div className="logo">
+          Movie App
+        </div>
+      </Link>
+      <div className="user-image">
+        <img src={"#"} alt="user" />
+      </div>
+    </div>
   )
 }
 
